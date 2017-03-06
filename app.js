a7a56ca16b3d990ae6b14fc8102a7b9a3705294c
Renzo Tomlinson <rttomlinson@gmail.com>
2017-03-06 05:22:45 +0000

    All catch-all error handler and 404 error handler

--- app.js
+++ app.js
@@ -7,17 +7,40 @@ let app = express();
 /*Serve static files from public directory*/
 app.use(express.static(path.join(__dirname, "public")));
 
+/*Set views directory*/
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'hbs');
+
+
+
+
 
 app.get("/", function(req, res) {
-    res.send("hello, world!");
+    res.render("index", { "title": "Hbs template" });
+});
+
+
+
+/* Handle 404 response. All other handlers should be above this*/
+app.use(function (req, res, next) {
+    let error = new Error("File can't be found");
+    error.status = 404;
+    next(error);
+});
+
+/** Error handler **/
+app.use(function (err, req, res, next) {
+    
+    
+    let errorMessage = err.message;
+    res.status(err.status || 500);
+    res.render("error", { "errorMessage": errorMessage });
+    
+    
 });
 
 
 
 
 
-
-
-app.listen(8080, function() {
-    console.log("App listening on 8080");
-});
+module.exports = app;
