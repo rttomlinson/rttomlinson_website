309cfc7546c0c1d6f77c585069061958dbaa2932
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-27 21:52:03 +0000

    Fix path to projects

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,24 @@ var router = express.Router();
 
 
 
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
     let promise = new Promise((resolve, reject) => {
-        http.get('http://rttomlinson-public-projects.herokuapp.com/', (res) => {
+        http.get('http://rttomlinson-public-projects.herokuapp.com/projects/all', (res) => {
             const statusCode = res.statusCode;
             const contentType = res.headers['content-type'];
-            
+
             let error;
-            
+
             if (statusCode !== 200) {
                 error = new Error(`Request failed.\n Status code: ${statusCode}`);
-            } else if (!/^application\/json/.test(contentType)) {
+            }
+            else if (!/^application\/json/.test(contentType)) {
                 error = new Error(`Invalid content type.\n Content type is: ${contentType}. Need JSON. Check request headers`);
             }
             if (error) {
                 reject(error);
             }
-            
+
             res.setEncoding('utf-8');
             let rawData = '';
             res.on('data', (chunk) => rawData += chunk);
@@ -31,12 +32,12 @@ router.use(function (req, res, next) {
                     let parsedData = JSON.parse(rawData);
                     resolve(parsedData);
                 }
-                catch(e) {
+                catch (e) {
                     console.log(`An error occured in parsing JSON try/catch: ${e.message}`);
                     reject(e);
                 }
             });
-        
+
         }).on('error', (e) => {
             console.log(`Error message caught by http get request: ${e.message}`);
             reject(e);
@@ -46,7 +47,7 @@ router.use(function (req, res, next) {
         res['myAPICalls'] = {};
         res.myAPICalls['personalProjects'] = data;
         next();
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.log(error.message);
         next();
         return;
@@ -57,7 +58,9 @@ router.use(function (req, res, next) {
 
 
 router.get('/', function(req, res, next) {
-    res.render('index', {"projects": res.myAPICalls.personalProjects});
+    res.render('index', {
+        "projects": res.myAPICalls.personalProjects
+    });
 });
 
 
